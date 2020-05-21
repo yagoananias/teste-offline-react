6634023ea6b7669b89dbaed319f78969c4f66824
Yago Ananias <yago_souza92@yahoo.com.br>
2020-05-21 01:48:37 -0300

    Iniciando o Projeto

---
+++ src/RootStack.js
@@ -0,0 +1,6 @@
+import { createAppContainer } from 'react-navigation'
+import { createStackNavigator } from 'react-navigation-stack'
+
+import LoginComponent from './LoginComponent'
+import SignUpComponent from './SignUpComponent'
+import SwiperComponent from './SwiperComponent'
